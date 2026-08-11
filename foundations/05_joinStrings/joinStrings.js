93f57f506d9953addfb140617ccc9e612de1e3a5
Avkzeus <167372322+Avkzeus@users.noreply.github.com>
2026-08-11 09:56:53 -0400

    Fix syntax error in greeting string concatenation

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -15,7 +15,7 @@ const lastName = "Stevenson";
 const thisYear = 1965;
 const birthYear = 1947;
 
-const greeting = "Hello! my name is" + firstName + lastName + "and I am" (thisYear - birthYear) + "years old";
+const greeting = "Hello! my name is" + firstName + lastName + "and I am" + (thisYear - birthYear) + "years old";
 // Do not change this
 module.exports = {
   firstName: typeof firstName === 'undefined' ? undefined : firstName,
